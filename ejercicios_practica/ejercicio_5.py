6412227949f0a6b27dd783ea9de7c85cdb5157d2
Ruben <87348517+rubensgonzalez@users.noreply.github.com>
2021-07-18 11:29:21 -0300

    Update ejercicio_5.py

--- ejercicios_practica/ejercicio_5.py
+++ ejercicios_practica/ejercicio_5.py
@@ -17,6 +17,12 @@ print('Ingrese palabra 2:')
 palabra_2 = str(input())
 
 # De la primera palabra tome las primeras tres letras, utilice el operador :
+arreglo1= palabra_1[0:3]
 # De la segunda palabra tome las primeras dos letras, utilice el operador :
+arreglo2= palabra_2 [0:2]
 # Formar una nueva palabra con los recortes solicitados
+palabra=arreglo1 + arreglo2
 # Imprima en pantalla los resultados
+print("De la primera palabra las primeras 3 letras:", arreglo1)
+print("De la segunda palabra las primeras 2 letras:", arreglo2)
+print("La palabra formada es :", palabra)
