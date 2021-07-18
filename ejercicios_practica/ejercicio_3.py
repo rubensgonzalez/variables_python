646e72b90e1affabaa7014733c05bf5e59d47dc3
Ruben <87348517+rubensgonzalez@users.noreply.github.com>
2021-07-18 11:09:07 -0300

    Update ejercicio_3.py

--- ejercicios_practica/ejercicio_3.py
+++ ejercicios_practica/ejercicio_3.py
@@ -19,8 +19,14 @@ apellido = str(input())
 
 # Imprima su nombre completo
 
+print(" Mi nombre completo es:", nombre," ",apellido)
+
 # Almacenar su nombre completo en una variable
 # nombre_completo = .....
+nombre_completo = nombre + apellido
 
 # Imprimir la cantidad de letras que posee su nombre completo
 # cantidad_letras = len(....)
+
+cantidad_letras = lent(nombre_completo)
+print("La cantidad de letras de mi nombre completo es:" , cantidad_letras)
