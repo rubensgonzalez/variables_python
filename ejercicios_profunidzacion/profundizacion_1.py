0552c018ad1ef73b8d92d039abb94056d5501c74
Ruben <87348517+rubensgonzalez@users.noreply.github.com>
2021-07-18 11:53:21 -0300

    Update profundizacion_1.py

--- ejercicios_profunidzacion/profundizacion_1.py
+++ ejercicios_profunidzacion/profundizacion_1.py
@@ -31,3 +31,28 @@ E) Exponente/Potencia
 
 print('¡Nuestra primera calculadora!')
 # Empezar aquí la resolución del ejercicio
+
+#A) Suma___
+print("Ingrese el primer numero real a sumar")
+numero_1 = float(input())
+print("Ingrese el segundo numero real a sumar")
+numero_2 = float(input())
+suma = numero_1 + numero_2
+print("La suma entre" , numero_1, "y" , numero_2 ,"es": suma )
+
+#b) Resta___
+resta = numero_1 - numero_2
+print("La resta entre" , numero_1, "y" , numero_2 ,"es": resta )
+
+#c) Multiplicacion___
+mult = numero_1 * numero_2
+print("La multiplicacion entre" , numero_1, "y" , numero_2 ,"es": mult )
+
+#d)División___
+divi = numero_1 / numero_2
+print("La division entre" , numero_1, "y" , numero_2 ,"es": divi )
+
+#e) Exponente___
+exp = numero_1 ** numero_2
+print("El exponente/potencia entre" , numero_1, "y" , numero_2 ,"es": exp )
+
